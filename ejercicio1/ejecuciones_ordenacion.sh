82a9109b879ab33363caf9a043fceaff47187c81
Alba <albamo@correo.ugr.es>
2019-10-02 18:25:21 +0200

    first exercise

---
+++ ejercicio1/ejecuciones_ordenacion.sh
@@ -0,0 +1,17 @@
+#!/bin/csh
+@ inicio = 100
+@ fin = 30000
+@ incremento = 500
+set ejecutable = burbuja
+set salida = tiempos_operacion.txt
+
+@ i=$inicio
+echo > $salida
+while ( $i < $fin )
+  echo Ejecución tam = $i
+  echo `./{$ejecutable} $i` >> $salida
+  @ i += $incremento
+  echo Ejecución tam = $i
+end
+
+
